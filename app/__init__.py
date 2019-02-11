f6cffdff346e10ed4df8da24fd5def1d482136e8
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-11 17:32:33 +0300

    Initialized bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 # Initializing application
 app = Flask(__name__)
+# Initializing flask extensions
+bootstrap.init_app(app)
 
 from app import views
+from app import error
