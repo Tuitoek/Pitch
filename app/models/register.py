825e1838585a50b03a4bb35de81e14e23e2aa402
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-12 09:33:52 +0300

    Updated models.py

---
+++ app/models/register.py
@@ -0,0 +1,7 @@
+class Register:
+    register=[]
+    def __init__(self,username,email,password,confirmpassword):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.confirmpassword = confirmpassword
