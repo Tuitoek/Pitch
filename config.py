dcd90a2abc5bee1983ca99f0bf0760f4abd5f171
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-12 21:32:16 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -1,9 +1,20 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:sarahjt12@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:sarahjt12@localhost/pitches'
+    SQLALCHEMY_TRACk_NOTIFICATIONS = False
     SECRET_KEY='Cake124'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    # MAIL_SERVER = 'sjtoek.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     pass
 
 
