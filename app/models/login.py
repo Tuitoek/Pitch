6dffa8545706bb10e98b1a8c4a4185f3aa2ad684
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-12 21:34:38 +0300

    Deleted the models folder

--- app/models/login.py
+++
@@ -1,6 +0,0 @@
-class Login:
-    login=[]
-    def __init__(self,username,password):
-        self.username = username
-        self.password = password
-        
--- app/models/register.py
+++
@@ -1,7 +0,0 @@
-class Register:
-    register=[]
-    def __init__(self,username,email,password,confirmpassword):
-        self.username = username
-        self.email = email
-        self.password = password
-        self.confirmpassword = confirmpassword
