68f27a6da97d3ae88d09467d9348a0afa8503e65
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-14 10:16:53 +0300

    Changed database URL

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_CHARCOAL_URL")
 
 
 class DevConfig(Config):
